aac2d740432dece96d9e238ad24aff6ed19aa6b2
SKNsiddharth <86350105+SKNsiddharth@users.noreply.github.com>
2024-02-28 10:07:47 +0530

    Add files via upload
    Activity 3 , 15 programs

---
+++ lakshey.java
@@ -0,0 +1,65 @@
+class Lakshey
+{
+	public static void main(String [] args)
+	
+	{
+		int a = 786;
+		System.out.println("-------------------------------------------------");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("6");
+		System.out.println("-------------------------------------------------");
+		
+		int b = 7+6+8;
+		System.out.println(b);
+		System.out.println("-------------------------------------------------");
+		
+		int c = 7*8*6;
+		System.out.println(c);
+		System.out.println("-------------------------------------------------");
+		
+		int d = 8+6;
+		System.out.println(d);
+		System.out.println("-------------------------------------------------");
+		
+		int e = 7;
+		System.out.println(e);
+		System.out.println("-------------------------------------------------");
+		
+		int f = 4*4 , g = 7*7 , h = 9*9 ;
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println("-------------------------------------------------");
+		
+		int i = 5*5*5 , j = 15*15*15 , k = 9*9*9 ;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println("-------------------------------------------------");
+		
+		int num1 =5*5*5*5;
+		int num2 =3*3*3*3*3*3;
+		int num3 =6*6;
+		int num4 =8*8*8;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println("-------------------------------------------------");
+		
+		double area = 4*5.6;
+		System.out.println(area);
+		System.out.println("-------------------------------------------------");
+		
+		double per = 2*(4.1+3.2);
+		System.out.println(per);
+		System.out.println("-------------------------------------------------");
+		
+		double per1 = 4*6;
+		double per2 = 6*6;
+		System.out.println(per1);
+		System.out.println(per2);
+		System.out.println("-------------------------------------------------");
+		
+	}
+}
